4c47742ce30d26abea2e239b199a80bcf6671158
zxp <zhangxiaopin1024@163.com>
2019-08-27 18:10:05 +0800

    add new file

--- LearningNote/LearnCPlusPlusOnMac/Chapter_1/source_code/practice_1_8.cpp
+++ LearningNote/LearnCPlusPlusOnMac/Chapter_1/source_code/practice_1_8.cpp
@@ -1,3 +1,18 @@
 /*
 *   Indicate which, if any, of the following output statement are legal:
 */
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "/*";      //合法
+  std::cout << "*/";      // 合法
+//  std::cout << /* "*/" */;  // 不合法 
+  std::cout << /*  "*/" /* "/*"  */;   //合法
+  return 0;
+}
+/*
+*  After you've predicted what will happen, test your answers by compiling a
+*  program with each of these statements. Correct any errors you encounter
+*/
