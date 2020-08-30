7384f703009b6e364b6c63ddeb54eccf0246bc43
wenxueanxianglai <942514383@qq.com>
2020-08-30 18:34:17 +0800

    add

--- LearningNote/LearnCPlusPlusOnMac/Chapter_2/source_code/test_1.cpp
+++ LearningNote/LearnCPlusPlusOnMac/Chapter_2/source_code/test_1.cpp
@@ -1,14 +1,41 @@
 #include<iostream>
+#include <string>
 
 using namespace std;
+class b
+{
+    static int a;
+    public:
+    void func()
+    {
+
+    }
+};
+
+int b::a = 0;
+
+enum color;
+enum color : unsigned char;
+class test;
+class test : public b
+{
+    using b::func;
+
+};
 
 int main()
 {
+    using std::string;
+
+    string   dddd;
+    ;
+
     unsigned int d= 12;
     float a = -111;
 
     cout << a + d << endl;
 
+    enum color;
 
     return 0;
 }
