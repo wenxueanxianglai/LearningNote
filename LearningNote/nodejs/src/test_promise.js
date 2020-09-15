92873064e699afbbffde7cd620ce91c2c5b4c705
zhangxiaopin <zhangxiaopin1024@163.com>
2020-09-15 14:45:47 +0800

    add

--- LearningNote/nodejs/src/test_promise.js
+++ LearningNote/nodejs/src/test_promise.js
@@ -1,10 +1,36 @@
-async function timeout() {
-    return "helloworld";
+function callback() {
+  console.log("Done");
 }
 
-timeout().then( (result) => {
-    console.log(result);
+console.log("before setTimeout()");
+setTimeout(callback, 1000);
+console.log("after setTimeout");
+
+
+function log(s) {
+    console.log(s);
+}
+
+function test(resolve, reject) {
+    var timeOut = Math.random() * 2;
+    log('set timeout to: ' + timeOut + ' seconds');
+    setTimeout(function() {
+        if(timeOut < 1) {
+            log('call resolve()..');
+            resolve('200OK');
+        }
+        else {
+            log('call reject()...');
+            reject('timeout in ' + timeOut + ' seconds.');
+        }
+    }, timeOut * 1000);
+}
+
+var p1 = new Promise(test);
+var p2 = p1.then(function (result) {
+    console.log('成功：' + result);
 });
 
-// console.log(timeout());
-console.log("neeeasd");
+var p3 = p2.catch(function(reason) {
+    console.log('失败：' + reason);
+});
