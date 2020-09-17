f5f3d1bd8ae030811a560abc78a756e9bd6c9e80
zhangxiaopin <zhangxiaopin1024@163.com>
2020-09-17 18:32:49 +0800

    add

--- LearningNote/webrtc2work/selenium/selenium.md
+++ LearningNote/webrtc2work/selenium/selenium.md
@@ -1,64 +1,84 @@
 # selenium
+
 ---
+
 ## link
+
 https://blog.csdn.net/qq_37546891/article/details/79038559
 
-
 ---
+
 ## tasklog
+
 ### 调通远程
+
 ```
 2020-9-16 14:15:47
 当前： 依旧不行
 任务： 尝试 统一所有的版本，看看是不是版本的问题
 ```
 
-
 ## 安装步骤：
+
 ### client 安装
+
 就是 webdriver 安装
+
 ```
 npm i selenium-webdriver@3.6.0
 ```
 
 ### driver 安装
-与本机使用的 chrome浏览器 版本要 一致！
+
+与本机使用的 chrome 浏览器 版本要 一致！
+
 ```
 https://sites.google.com/a/chromium.org/chromedriver/
 ```
 
 ### grid 下载
+
 下载 稳定版
+
 ```
 https://www.selenium.dev/downloads/
 ```
 
 ## 运行
+
 当前仅在单个机器上运行成功
 
-
 ## 连接
+
 http://www.docin.com/p-698130153.html
 
 https://www.cnblogs.com/hanxiaobei/p/7352794.html
 
-
 http://www.voidcn.com/article/p-qtkihoyu-bvu.html
 
 #### 已完成
-调通 client端 向 grid hub节点发送脚本， grid hub 执行。
+
+调通 client 端 向 grid hub 节点发送脚本， grid hub 执行。
 
 #### 尚未解决
-如何 通过 js 获取所有的node节点
-如果通过获取的node节点去 精准控制
+
+如何 通过 js 获取所有的 node 节点
+如果通过获取的 node 节点去 精准控制
 
 #### 意外遇到的问题
+
 当前尚未搜索到能够行之有效的方法
 
 ```
 cannot reach the node for 2 tries
 ```
-node显示：
+
+node 显示：
+
 ```
 01:53:39.064 INFO [SelfRegisteringRemote$1.run] - Couldn't register this node: The hub is down or not responding: Failed to connect to /192.168.0.105:4444
 ```
+
+https://github.com/SeleniumHQ/selenium/blob/selenium-3.141.59/java/server/src/org/openqa/grid/common/defaults/DefaultNodeWebDriver.json
+
+https://github.com/SeleniumHQ/selenium/tree/selenium-3.141.59/java/server/src/org/openqa/grid/common/defaults
--- LearningNote/webrtc2work/selenium/src/test_self.js
+++ LearningNote/webrtc2work/selenium/src/test_self.js
@@ -1,14 +1,37 @@
-const Builder = require('selenium-webdriver');
- 
+const webdriver = require("selenium-webdriver");
+const fs = require("fs");
+
 (async function example() {
-  let driver = await new Builder().forBrowser('chrome')
-  .usingServer('http://192.168.0.105:4444/wd/hub')                    
-  .build();
+  let driver = new webdriver.Builder()
+    .forBrowser("chrome")
+    .usingServer("http://192.168.1.119:4444/wd/hub")
+    .build();
+
   try {
-    await driver.get('http://www.baidu.com/');
-  //  await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
- ///   await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    await driver.get("http://www.baidu.com/");
+
+    var str = "";
+    await fs.readFile("./get_ip.js", function (error, data) {
+      if (error) {
+        console.log("read File failed");
+      } else {
+        str += data.toString();
+        console.log(str, str.length);
+      }
+    });
+
+    await driver.executeScript('alert("aaa");');
+    //    await (await driver).executeScript();
+    await (await driver).sleep(4000);
+
+    // let sess = (await driver).getWindowHandle();
+    // console.log(sess);
+
+    // let ur = (await driver).getCurrentUrl();
+    //console.log(ur);
+    //  await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+    ///   await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
   } finally {
-   // await driver.quit();
+    await driver.quit();
   }
 })();
