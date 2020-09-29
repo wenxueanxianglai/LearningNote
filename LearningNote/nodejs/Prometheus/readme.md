ab9855cbd229c9e32ed8d1febc114b90ddf93b41
zhangxiaopin <zhangxiaopin1024@163.com>
2020-09-29 18:20:10 +0800

    addd

---
+++ LearningNote/nodejs/Prometheus/readme.md
@@ -0,0 +1,8 @@
+## 任务记录
+---
+## 验证性测试：
+```
+目的： 测试 同一个 metrics 如何发到 pushgateway
+
+ jobName: 'test', groupings: { instance: 'xxxvalue' }           // xxxvalue 是instance
+```
---
+++ LearningNote/nodejs/Prometheus/src/test_metrics.js
@@ -0,0 +1,40 @@
+let client = require('prom-client');
+
+class SingletonApple {
+    constructor() {
+        this.gauge =  new client.Gauge(
+            {
+                name: 'metric_name',
+                help: 'metric_help',
+                labelNames: ["id"]
+            });
+    }
+
+    static getInstance() {
+        if(!this.instance) {
+            this.instance = new SingletonApple();
+        }
+        return this.instance;
+    }
+
+  }
+
+// const gauge = new client.Gauge(
+//     {
+//         name: 'metric_name',
+//         help: 'metric_help',
+//         labelNames: ["id"]
+//     });
+
+
+SingletonApple.getInstance().gauge.labels("1111aaaa").set(10); // Set to 10
+SingletonApple.getInstance().gauge.labels("2222aaaa").set(10); // Set to 10
+
+let gateway = new client.Pushgateway('http://127.0.0.1:9091');
+gateway.pushAdd({ jobName: 'test', groupings: { instance: 'xxxvalue' } }, function (      // , groupings: {key : 'myname'}      // 这个就是label
+    err,
+    resp,
+    body
+) {
+    console.log(resp);
+  });
