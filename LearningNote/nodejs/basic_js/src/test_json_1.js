912b530fab1f9b0c287cab0ffefb04fdc0885d8c
zhangxiaopin <zhangxiaopin1024@163.com>
2020-10-10 18:11:53 +0800

    aaa

---
+++ LearningNote/nodejs/basic_js/src/test_json_1.js
@@ -0,0 +1,44 @@
+let wr = {
+    id : '1234',
+    str : 'hahahah'
+};
+
+
+let wwr = {
+    id: '1234222',
+    rrr: 'sertt',
+    str : 'hahahah111111'
+};
+
+let arrwr = [wr, wwr ];
+console.log(arrwr);
+
+let index_0 = arrwr.findIndex(function (elem) {
+    return elem.id == '1234'
+})
+
+let index_1 = arrwr.findIndex(function (elem) {
+    return elem.id == '1234222'
+})
+
+arrwr.splice(index_0, 1);
+
+let mapwr = new Map();
+mapwr.set(wr.id, wr);
+mapwr.set(wwr.id, wwr);
+
+
+mapwr.forEach((value, key) => { 
+    console.log(value);
+});
+
+console.log(mapwr);
+
+mapwr.delete(wwr.id);
+
+console.log(mapwr);
+mapwr.forEach((value, key) => { 
+    console.log(value);
+});
+
+
