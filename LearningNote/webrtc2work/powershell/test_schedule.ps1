1f35150d3941e34c3809e38035de9a6c7f45deef
张小品 <zhangxp@butel.com>
2020-11-14 00:13:36 +0800

    添加

--- LearningNote/webrtc2work/powershell/test_schedule.ps1
+++ LearningNote/webrtc2work/powershell/test_schedule.ps1
@@ -1,3 +1,9 @@
 ﻿$mySchedule = Register-ScheduledJob -Name start_node -ScriptBlock {
     D:\test_selenium\node\renode.ps1
 }
+
+Start-Job -Name hub -ScriptBlock {taskkill /f /im java.exe;
+cd D:\test_selenium\hub
+java -jar selenium-server-standalone-3.141.59.jar -role hub;}
+
+$mySchedule 
