acca8ade3a3b461a55f80f9bc217e690b2cac202
张小品 <zhangxp@butel.com>
2020-11-15 17:50:27 +0800

    add

---
+++ LearningNote/webrtc2work/powershell/src/testRemote127.ps1
@@ -0,0 +1,14 @@
+﻿$computerName = 'test1', 'test2'
+foreach ($Computer in $ComputerName) {
+ Write-Host $Computer
+}
+
+
+function Get-NodeSession {
+    param(
+        $computerName,
+        $computt
+    )
+
+    
+}
